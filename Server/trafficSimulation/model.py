eb76755b1275557da365b85bc6e19437747dbde6
sergiozuckermann <a01024831@itesm.mx>
2023-11-30 22:20:14 -0600

    Unity fixes

--- Server/trafficSimulation/model.py
+++ Server/trafficSimulation/model.py
@@ -140,26 +140,26 @@ class CityModel(Model):
                 self.spawn()
         self.schedule.step()
         
-        if self.steps % 100 == 0:
+        # if self.steps % 100 == 0:
             
-            url = "http://52.1.3.19:8585/api/"
-            endpoint = "attempts"
+        #     url = "http://52.1.3.19:8585/api/"
+        #     endpoint = "attempts"
 
-            data = {
-                "year" : 2023,
-                "classroom" : 302,
-                "name" : "Santiago y Sergio",
-                "num_cars": self.arrived
-            }
+        #     data = {
+        #         "year" : 2023,
+        #         "classroom" : 302,
+        #         "name" : "Santiago y Sergio",
+        #         "num_cars": self.arrived
+        #     }
 
-            headers = {
-                "Content-Type": "application/json"
-            }
+        #     headers = {
+        #         "Content-Type": "application/json"
+        #     }
 
-            response = requests.post(url+endpoint, data=json.dumps(data), headers=headers)
+        #     response = requests.post(url+endpoint, data=json.dumps(data), headers=headers)
 
-            print("Request " + "successful" if response.status_code == 200 else "failed", "Status code:", response.status_code)
-            print("Response:", response)
+        #     print("Request " + "successful" if response.status_code == 200 else "failed", "Status code:", response.status_code)
+        #     print("Response:", response)
         if self.steps % 1000 == 0:
             self.running = False
         # check if cars are cras
